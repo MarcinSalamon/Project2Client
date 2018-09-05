792ef543a80e8b712ce1af17e64544264b17caab
danielsidhu <40637004+danielsidhu@users.noreply.github.com>
2018-09-04 23:24:36 -0400

    models added

---
+++ project2/src/app/models/users.ts
@@ -0,0 +1,11 @@
+export class Users {
+    uId: number;
+    fname: string;
+    lname: string;
+    hash: string;
+    salt: string;
+    email: string;
+    username: string;
+    friendsList: string;
+    onlineStatus: boolean;
+}
