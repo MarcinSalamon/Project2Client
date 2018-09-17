6b85cd79aab0f909eb243c504c511dccf1fda04c
Stephen <stephenh.email@gmail.com>
2018-09-16 21:29:59 -0400

    major update

--- project2/src/app/services/user.service.ts
+++ project2/src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { OnlineStatus } from './../models/online-status';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { Message } from '../models/message';
+import { Conversation } from '../models/conversation';
 import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -57,16 +58,47 @@ export class UserService {
     return this.http.get<User[]>(environment.apiUrl + 'user');
   }
 
-  logoutStatus() {
+  logoutStatus(uId, username, email, fname, lname, onlineStatus) {
     console.log('In UserService.logoutStatus()');
-    const json = '';
-    return this.http.post<User[]>(environment.apiUrl + 'logout', json, HTTP_OPTIONS);
+    const user = {
+      uId: uId,
+      username: username,
+      email: email,
+      fname: fname,
+      lname: lname,
+      onlineStatus: onlineStatus
+    };
+    const json = JSON.stringify(user);
+    console.log(user);
+    return this.http.put<User>(environment.apiUrl + 'user/' + user.uId, json, HTTP_OPTIONS);
   }
 
-  getAllMessages() {
+  getAllMessages(cId) {
     console.log('In UserService.getAllMessages()');
     const json = '';
-    return this.http.get<Message[]>(environment.apiUrl + 'conversation/2/message');
+    console.log(cId);
+    return this.http.get<Message[]>(environment.apiUrl + 'conversation/' + cId + '/message');
+  }
+
+  sendMessage(message) {
+    console.log('In UserService.sendMessage()');
+    const json = JSON.stringify(message);
+    return this.http.post<Message>(environment.apiUrl + 'conversation/' + message.conversationId + '/message', json, HTTP_OPTIONS);
+  }
+
+  createConversation(id1, id2) {
+    console.log('in UserService.createConversation');
+    const ids = {
+      uId1: id1,
+      uId2: id2
+    };
+    const conv = null;
+    const json = JSON.stringify(ids);
+    return this.http.post<Conversation>(environment.apiUrl + 'conversation', json, HTTP_OPTIONS);
+  }
+
+  getConversationsByUId(id) {
+    return this.http.get<Conversation[]>(environment.apiUrl + 'user/' + id + '/conversation');
   }
 
 }
